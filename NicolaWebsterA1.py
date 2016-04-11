06e6dfb124101c5b2bc33eb7cc60cf507eadf94c
Nicola Webster <nicola.webster@my.jcu.edu.au>
2016-04-11 15:05:26 +1000

    my assessment

---
+++ NicolaWebsterA1.py
@@ -0,0 +1,426 @@
+name = 'Items for Hire - by Nicola Webster'
+print(name)
+name = input('Welcome, please enter  your name:')
+print('Welcome', name)
+
+L = '(L)list all items'
+H = '(H)ire an item'
+R = '(R)return an item'
+A = '(A)add new item to stock'
+Q = '(Q)uit'
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input ("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input ("Enter choice:")
+
+
+if L:
+    print ("All items on file (* indicates item is currently out):")
+    print("0 - Rusty Bucket (40L bucket - quite rusty) = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo) = $ 195.00")
+    print("2 - Thermomix (TM-31)                   = $ 25.50 *")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input ("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input ("Enter choice:")
+
+if H:
+    print("0 - Rusty Bucket (40L bucket - quite rusty) = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo)     = $ 195.00")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input ("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input ("Enter choice:")
+
+if H:
+    print("0 - Rusty Bucket (40L bucket - quite rusty) = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo)     = $ 195.00")
+
+number = int(input("Enter the item number to hire"))
+if 0:
+    print("Rusty Bucket (40L bucket - quite rusty)      = $ 0.00")
+else:
+    print("Golf Cart (Tesla powered 250 turbo)         = $ 195.00")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if L:
+    print("All items on file (* indicates item is currently out):")
+    print("0 - Rusty Bucket (40L bucket - quite rusty)  = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo)      = $ 195.00*")
+    print("2 - Thermomix (TM-31)                        = $ 25.50 *")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if H:
+    print("0 - Rusty Bucket (40L bucket - quite rusty)     = $ 0.00")
+
+number = int(input("Enter the item number to hire"))
+if 1:
+    print ("That item is no longer avaiable for hire")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if R:
+    print("1 - Golf Cart (Tesla powered 250 turbo)   = $ 195.00")
+    print("2 - Thermomix (TM-31)                     = $ 25.50")
+
+number = int(input("Enter the item number to return"))
+if 1:
+    print ("This itnme has been returned")
+else:
+    if 2:
+        print ("This itnme has been returned")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if L:
+    print("All items on file (* indicates item is currently out):")
+    print("0 - Rusty Bucket (40L bucket - quite rusty)  = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo)      = $ 195.00*")
+    print("2 - Thermomix (TM-31)                        = $ 25.50 ")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if R:
+    print("1 - Golf Cart (Tesla powered 250 turbo)   = $ 195.00")
+
+number = int(input("Enter the item number to return"))
+if 1:
+    print ("This itnme has been returned")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if L:
+    print("All items on file (* indicates item is currently out):")
+    print("0 - Rusty Bucket (40L bucket - quite rusty)  = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo)      = $ 195.00")
+    print("2 - Thermomix (TM-31)                        = $ 25.50 ")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if R:
+    print ("No items are currentl on hire")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if A:
+    item_name = input('Enter the item name:')
+    print('The item is ', item_name)
+    description = input('What is the item:')
+    print(description)
+    price_per_day = input('Enter the price per day: $')
+    print(price_per_day)
+    print(item_name, ',', description, ',', '$', price_per_day, "now available for hire")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if L:
+    print("All items on file (* indicates item is currently out):")
+    print("0 - Rusty Bucket (40L bucket - quite rusty)  = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo)      = $ 195.00")
+    print("2 - Thermomix (TM-31)                        = $ 25.50 ")
+    print("3 - ", item_name, '(', description, ')', '                  = $', price_per_day,".00")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if H:
+    print("0 - Rusty Bucket (40L bucket - quite rusty)  = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo)      = $ 195.00")
+    print("2 - Thermomix (TM-31)                        = $ 25.50 ")
+    print("3 - ", item_name, '(', description, ')', '                  = $', price_per_day, ".00")
+
+number = int(input("Enter the item number to hire"))
+if 0:
+    print("Rusty Bucket (40L bucket - quite rusty)      = $ 0.00")
+elif 3:
+    print("3 - ", item_name, '(', description, ')', '                     = $', price_per_day, ".00")
+elif 2:
+    print("2 - Thermomix (TM-31)                        = $ 25.50 ")
+else:
+    print("Golf Cart (Tesla powered 250 turbo)         = $ 195.00")
+    print("Item has been hired")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if L:
+    print("All items on file (* indicates item is currently out):")
+    print("0 - Rusty Bucket (40L bucket - quite rusty)  = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo)      = $ 195.00")
+    print("2 - Thermomix (TM-31)                        = $ 25.50 ")
+    print("3 - ", item_name, '(', description, ')', '                  = $', price_per_day, ".00*")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if A:
+    item_name = input('Enter the item name:')
+    print('The item is ', item_name)
+    description = input('What is the item:')
+    print(description)
+    price_per_day = input('Enter the price per day: $')
+    print(price_per_day)
+    print(item_name, ',', description, ',', '$', price_per_day, "now available for hire")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+if L:
+    print("All items on file (* indicates item is currently out):")
+    print("0 - Rusty Bucket (40L bucket - quite rusty)  = $ 0.00")
+    print("1 - Golf Cart (Tesla powered 250 turbo)      = $ 195.00")
+    print("2 - Thermomix (TM-31)                        = $ 25.50 ")
+    print("3 - ", item_name, '(', description, ')', '                  = $', price_per_day, ".00*")
+    print("4 - ", item_name, '(', description, ')', '                  = $', price_per_day, ".00")
+
+menu = 'Menu:'
+print(menu)
+print(L)
+print(H)
+print(R)
+print(A)
+print(Q)
+
+choices = {'L', 'H', 'R', 'A', 'Q'}
+choice = input("Enter choice:")
+
+if choice in choices:
+    print("Your choice was", choice)
+else:
+    print("Renter your choice")
+    choice = input("Enter choice:")
+
+ if Q:
+     print ("Have a nice day!")
